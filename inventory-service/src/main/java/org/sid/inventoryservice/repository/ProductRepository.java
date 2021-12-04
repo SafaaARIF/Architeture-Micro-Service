ef3b2b084e551b3f75e2b3a590e745666f6439b8
SafaaARIF <81268119+SafaaARIF@users.noreply.github.com>
2021-12-04 02:15:39 +0100

    Add files via upload

---
+++ inventory-service/src/main/java/org/sid/inventoryservice/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package org.sid.inventoryservice.repository;
+
+import org.sid.inventoryservice.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
+
+@RepositoryRestController
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
